19d673fb64a36db1cb07cdf988f181a9c0e8c8a1
anjabnajib-creator <anjab.najib@gmail.com>
2026-07-23 21:32:32 +0700

    Create     script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+const questions = [
+{
+emoji:"👋",
+question:"Nama panggilan yang paling sering dipakai?"
+},
+
+{
+emoji:"🍗",
+question:"Makanan yang gak pernah bikin bosen?"
+},
+
+{
+emoji:"☕",
+question:"Minuman favorit kalau lagi nongkrong?"
+},
+
+{
+emoji:"📍",
+question:"Tempat favorit buat healing atau melepas penat?"
+},
+
+{
+emoji:"🎵",
+question:"Lagu atau genre musik yang lagi sering didenger akhir-akhir ini?"
+},
+
+{
+emoji:"🌙",
+question:"Weekend ideal versi Andri?"
+},
+
+{
+emoji:"😆",
+question:"Kebiasaan unik yang sering dilakukan tanpa sadar?"
+},
+
+{
+emoji:"💸",
+question:"Hal paling impulsif yang pernah dilakukan?"
+},
+
+{
+emoji:"🎒",
+question:"Barang yang wajib dibawa ke mana-mana?"
+},
+
+{
+emoji:"🎬",
+question:"Film atau series favorit sepanjang masa?"
+},
+
+{
+emoji:"❤️",
+question:"Love language utama?"
+},
+
+{
+emoji:"🟢",
+question:"Menurut Andri, green flag seseorang itu apa?"
+},
+
+{
+emoji:"🚩",
+question:"Red flag yang bikin langsung ilfeel?"
+},
+
+{
+emoji:"😶",
+question:"Kalau lagi bad mood biasanya lebih suka gimana?"
+},
+
+{
+emoji:"🤝",
+question:"Hal yang paling gak bisa ditoleransi dalam hubungan?"
+},
+
+{
+emoji:"💭",
+question:"Pelajaran hidup paling berharga yang pernah didapat?"
+},
+
+{
+emoji:"🏡",
+question:"Apa arti 'safe space' menurut Andri?"
+},
+
+{
+emoji:"✈️",
+question:"Kalau bisa liburan bareng pasangan, pengen ke negara mana?"
+},
+
+{
+emoji:"😊",
+question:"Kesan pertama waktu kenal Jabun?"
+},
+
+{
+emoji:"🤍",
+question:"Terakhir... apa yang bikin Andri masih mau lanjut kenal sama Jabun?"
+}
+];
